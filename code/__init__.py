c01adff43310fd24009d5b050ba58f75905684c0
Srujan Ponnur <srujanponnur@gmail.com>
2022-08-25 21:27:51 -0400

    added add, multiply, divide and sum operations

---
+++ code/__init__.py
@@ -0,0 +1,15 @@
+class Operation:
+    def add(self, input1, input2):
+        return input1 + input2
+
+    def add(self, input1, input2):
+        return input1 * input2
+
+    def subtract(self, input1, input2):
+        return input1 - input2
+
+    def divide(self, input1, input2):
+        if input2 == 0:
+            pass
+        else:
+            return input1 / input2
