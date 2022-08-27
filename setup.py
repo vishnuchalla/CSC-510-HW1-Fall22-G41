5524809cecfafc0b29b51a34ad2bfc55efa6007d
Sairam Sakhamuri <svsakham@libltlnd-40488.lan>
2022-08-26 22:34:31 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='ncsu_se_group41',
+      version='1.0',
+      description='CSC 510 Fall 2022 Group 41',
+      author='Members of group 4',
+      author_email='svnsairam.sakhamuri@gmail.com',
+      license='MIT',
+      packages=['yet to come'],
+      install_requires=[
+          'python',
+      ],
+     )
