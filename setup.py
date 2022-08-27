0021677db09d2733c7663f55d2a0199ef4048fb5
Sairam Sakhamuri <svsakham@libltlnd-40488.lan>
2022-08-26 22:39:41 -0400

    Added setup file

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from setuptools import setup
 
 setup(name='ncsu_se_group41',
       version='1.0',
-      description='CSC 510 Fall 2022 Group 41',
+      description='CSC 510 Fall 2022 Group 41, the repo depicts how to implement a good repository in github',
       author='Members of group 4',
       author_email='svnsairam.sakhamuri@gmail.com',
+      url='https://github.com/vishnuchalla/CSC-510-HW1-Fall22-G41',
       license='MIT',
       packages=['yet to come'],
-      install_requires=[
+      classifiers=[
+		"MIT License",
+		"Programming Languages :: Python",
+		"Topic :: CSC 510: Software Engineering",
+	    ],
+	    license="MIT",
+        install_requires=[
           'python',
-      ],
+        ],
+
      )
